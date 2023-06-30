d9d46ca5da3adc1b8a85b69a4f00cd4b2ddae9a8
vxc333 <you@example.com>
2023-06-30 03:49:22 -0300

    Adicionando alguns "Else"

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -101,46 +101,48 @@ module.exports = class BinarySearchTree {
     }
     if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
       return this.searchNode(node.left, key);
-    }
-    if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
       return this.searchNode(node.right, key);
+    } else {
+      return true;
     }
-    return true;
   }
   remove(key) {
     this.root = this.removeNode(this.root, key);
   }
   removeNode(node, key) {
     if (node == null) {
-      return undefined;
+      return null; 
     }
     if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
       node.left = this.removeNode(node.left, key);
       return node;
-    }
-    if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
       node.right = this.removeNode(node.right, key);
       return node;
-    }
-    if (node.left == null && node.right == null) {
-      node = undefined;
+    } else {
+      //Caso 1
+      if (node.left == null && node.right == null) {
+        node = null;
+        return node;
+      }
+      //Caso 2
+      if (node.left == null) {
+        node = node.right;
+        return node;
+      } else if (node.right == null) {
+        node = node.left;
+        return node;
+      }
+      //Caso 3
+      const aux = this.minNode(node.right);
+      node.key = aux.key;
+      node.right = this.removeNode(node.right, aux.key);
       return node;
     }
-    if (node.left == null) {
-      node = node.right;
-      return node;
-    }
-    if (node.right == null) {
-      node = node.left;
-      return node;
-    }
-    const aux = this.minNode(node.right);
-    node.key = aux.key;
-    node.right = this.removeNode(node.right, aux.key);
-    return node;
   }
 };
 
-
-// 
-const printNode = (value)=> console.log(value)
+//
+// const printNode = (value) => console.log(value);
+// const tree = require("./binarySearchTree.js");
